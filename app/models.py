a24ca86083fb689b0378fe4ea2a15c1397378bcf
umwalidiane <umwalidiane69@gmail.com>
2019-09-22 16:41:38 +0200

    styled  user class

--- app/models.py
+++ app/models.py
@@ -13,16 +13,16 @@ class Source:
         self.country = country
 
 
-class Articles:
-   '''
-   Articles class to define articles objects
-   '''
-   def __init__(self,id,author,title,description,url,image,date):
-       self.id = id
-       self.author = author
-       self.title = title
-       self.description = description
-       self.url = url
-       self.image = image
-       self.date = date
+# class Articles:
+#    '''
+#    Articles class to define articles objects
+#    '''
+#    def __init__(self,id,author,title,description,url,image,date):
+#        self.id = id
+#        self.author = author
+#        self.title = title
+#        self.description = description
+#        self.url = url
+#        self.image = image
+#        self.date = date
        
